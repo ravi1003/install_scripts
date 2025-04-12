bbc1da7072052d2e41487faf1b56f869ffd11f57
Ravi <user@users-MacBook-Pro.local>
2025-04-12 20:05:16 +0530

    made changes in jenkins file

--- jenkins.sh
+++ jenkins.sh
@@ -26,3 +26,6 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y 
 sudo apt-get install jenkins -y
+
+
+#learn Devops
